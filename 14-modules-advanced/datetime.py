bd8b9969a5604a8420c839a1bd6e55c85e51b1a2
unknown <geoffdotson@gmail.com>
2019-10-15 02:27:42 -0500

    Added todos

---
+++ 14-modules-advanced/datetime.py
@@ -0,0 +1,2 @@
+# https://www.programiz.com/python-programming/datetime
+# todo more links whole section
---
+++ 15-advanced-py-objects-data-structures/do.py
@@ -0,0 +1,4 @@
+# https://www.programiz.com/python-programming/nested-dictionary
+# https://www.programiz.com/python-programming/array
+# https://www.programiz.com/python-programming/matrix
+# https://www.programiz.com/python-programming/list-comprehension
